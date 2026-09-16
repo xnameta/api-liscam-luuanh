0771c614bd1f413af22e7af156bc4c5093a9809e
xnameta <brgg56ksbf@privaterelay.appleid.com>
2026-09-17 00:41:33 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,3 +6,4 @@ load_dotenv()
 IMGBB_API_KEY = os.getenv("IMGBB_API_KEY", "")
 IMGBB_UPLOAD_URL = "https://api.imgbb.com/1/upload"
 IMAGEKIT_PRIVATE_KEY = os.getenv("IMAGEKIT_PRIVATE_KEY", "")
+FREEIMAGE_API_KEY = os.getenv("FREEIMAGE_API_KEY", "")
