f72598429dd8baa58bf878cef0213653259cff17
xnameta <brgg56ksbf@privaterelay.appleid.com>
2026-09-17 00:09:44 +0700

    Update base.py

--- app/storage/base.py
+++ app/storage/base.py
@@ -4,9 +4,22 @@ from abc import ABC, abstractmethod
 class StorageProvider(ABC):
 
     @abstractmethod
-    async def upload(self, file_bytes: bytes, filename: str, content_type: str):
+    async def upload(
+        self,
+        file_bytes: bytes,
+        filename: str,
+        content_type: str
+    ):
         pass
 
     @abstractmethod
     async def delete(self, file_id: str):
+        pass
+
+    @abstractmethod
+    async def get_info(self, file_id: str):
+        pass
+
+    @abstractmethod
+    async def health_check(self):
         pass
